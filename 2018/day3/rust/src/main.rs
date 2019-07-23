8f93cfac97c1bf27659738bbae5c35f3f8781ac0
aptend <crescentwhale@hotmail.com>
2019-07-23 21:18:08 +0800

    day3 rust

--- 2018/day3/rust/src/main.rs
+++ 2018/day3/rust/src/main.rs
@@ -1,36 +1,63 @@
-#[macro_use] extern crate lazy_static;
 use std::error::Error;
 use std::str::FromStr;
+use std::collections::HashMap;
 use std::io::{self, Read, Write};
 
+// 2018 edition
 use regex::Regex;
+use lazy_static::lazy_static;
 
-type Result<T> = std::result::Result<T, Box<Error>>;
+
+macro_rules! err {
+    ($($e:tt)*) => {
+        Err(Box::<Error>::from(format!($($e)*))) //具体Result类型会在函数中利用返回值推断
+    };
+}
+
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>; //dyn显示表明Error是trait
+type Grid = HashMap<(u32, u32), u32>;
 
 fn main() -> Result<()> {
     let mut buf = String::new();
     io::stdin().read_to_string(&mut buf)?;
-    let mut claims: Vec<Claim> = vec![];
 
+    let mut claims: Vec<Claim> = vec![];
     for line in buf.lines() {
         //let claim = line.parse()?; 这样也可以，但传递的错误信息不好看
         let claim = line.parse().or_else(|err| {
-            Err(Box::<Error>::from(format!("failed to parse '{:?}': {}", line, err)))
+            err!("failed to parse {:?}: {}", line, err)
         })?;
         claims.push(claim);
     }
-    // part1(&buf)?;
-    // part2(&buf)?;
+
+    let mut grid = Grid::new();
+    for claim in &claims { // 等效于 in claims.iter() {
+        for (x, y) in claim.iter_points() {
+            *grid.entry((x, y)).or_default() += 1;
+        }
+    }
+
+    part1(&grid)?;
+    part2(&claims, &grid)?;
     Ok(())
 }
 
-fn part1(input: &str) -> Result<()> {
-    writeln!(io::stdout(), "{}", 42)?;
+fn part1(grid: &Grid) -> Result<()> {
+    // values借用，filter再借用，所以&&, 这在filter的文档中有记录
+    let count = grid.values().filter(|&&count| count > 1).count();
+    writeln!(io::stdout(), "contested points: {}", count)?;
     Ok(())
 }
 
-fn part2(input: &str) -> Result<()> {
-    Err(From::from("could not find two correct box ids"))
+fn part2(claims: &[Claim], grid: &Grid) -> Result<()> {
+    for claim in claims { // 这里的claims本身就是借用
+        if claim.iter_points().all(|p| grid[&p] == 1){
+            writeln!(io::stdout(), "uncontested claim: {}", claim.id)?;
+            return Ok(());
+        }
+    }
+    err!("could not find uncontested claim")
 }
 
 #[derive(Debug)]
@@ -82,24 +109,24 @@ impl<'c> Iterator for IterPoints<'c> {
 
 // parse使用的trait，没有生命周期参数，所以不能转换存在借用的结构
 impl FromStr for Claim {
-    type Err = Box<Error>;
+    type Err = Box<dyn Error>; // 转换成统一的错误类型，一个trait 对象
 
     fn from_str(s: &str) -> Result<Claim> {
         // 编译期函数执行(CTFE)受限的 work around
         // 运行时执行且执行一次，结果进入只读内存区
         lazy_static! {
-            static ref RE: Regex = Regex::new(r"(?x) //忽略空格并允许行#注释
-                \# #转义开始的'#'
+            static ref RE: Regex = Regex::new(r"(?x) #忽略空格并允许注释
+                \#  # 转义掉开头的#号
                 (?P<id>[0-9]+)
                 \s+@\s+
-                (?P<x>[0-9]+),(?P<y>[0-9]+)):
+                (?P<x>[0-9]+),(?P<y>[0-9]+):
                 \s+
                 (?P<width>[0-9]+)x(?P<height>[0-9]+)
             ").unwrap();
         }
 
         let caps = match RE.captures(s) {
-            None => return Err(Box::<Error>::from("unrecoginzed claim")),
+            None => return err!("unrecoginzed claim"),
             Some(caps) => caps,
         };
 
